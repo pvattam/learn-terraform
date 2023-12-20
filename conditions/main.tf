c15f4029b6fbab2dd1623675f3b0969803b7b3b0
Pushpak Vattam <vattampushpak243@gmail.com>
2023-12-20 08:09:09 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roles/prometheus/tasks/main.yml
    #	roles/prometheus/templates/prometheus.yml

--- conditions/main.tf
+++ conditions/main.tf
@@ -2,7 +2,12 @@ variable "instance_type" {
   default = null
 }
 
+variable "create_instance" {
+  default = false
+}
+
 resource "aws_instance" "test" {
+  count = var.create_instance ? 1 : 0
   ami = "ami-03265a0778a880afb"
   instance_type = var.instance_type == null ? "t3.micro" : var.instance_type
 }
